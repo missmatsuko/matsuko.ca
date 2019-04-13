a9bc976f5d85ed2ee065e3188eb7b02ce147075d
Matsuko Friedland <info@matsuko.ca>
2019-04-13 14:51:20 -0700

    Remove unused build packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
@@ -92,12 +91,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
-      resolve:'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true,            // Activates purging in npm run develop
-      },
-    }, // must be after other CSS plugins
     'gatsby-plugin-netlify-cache',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
