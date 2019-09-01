7d5184a5c2bd91874d5506f751ad5f9521dbdbec
Matsuko Friedland <info@matsuko.ca>
2019-09-01 12:51:24 -0700

    Clear out repo for new build

--- src/cms/cms.js
+++
@@ -1,7 +0,0 @@
-import CMS from 'netlify-cms'
-
-import PagePreview from './preview-templates/PagePreview'
-import ProjectPreview from './preview-templates/ProjectPreview'
-
-CMS.registerPreviewTemplate('page', PagePreview)
-CMS.registerPreviewTemplate('project', ProjectPreview)
