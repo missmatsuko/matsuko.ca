aa559b45210c7ebf9c1f83b0c76c04e863dbf8c5
Matsuko Friedland <info@matsuko.ca>
2020-02-09 22:05:34 -0800

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,50 @@
+<template>
+  <component
+    :is="type"
+    to="to"
+    class="button"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+import Link from '~/components/Link.vue';
+
+export default {
+  name: 'Button',
+  components: {
+    Link,
+  },
+  props: {
+    to: {
+      required: false,
+      default: '',
+      type: String,
+    },
+  },
+  computed: {
+    isLink() {
+      console.log(this.to);
+      return this.to.length;
+    },
+    type() {
+      return this.isLink ? 'Link' : 'button';
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .button {
+    display: inline-block;
+    text-align: center;
+    text-decoration: none;
+    font-weight: bold;
+    padding: var(--spacing-xs) var(--spacing-md);
+    background-color: transparent;
+    color: currentColor;
+    border: 2px solid currentColor;
+    border-radius: var(--spacing-xs);
+  }
+</style>
