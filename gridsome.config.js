62901af8c4c195a85d6562b169630bb228b1c4eb
Matsuko Friedland <info@matsuko.ca>
2021-12-23 16:51:15 -0800

    Fix marked import

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 const autoprefixer = require('autoprefixer');
-const marked = require('marked');
+const { marked } = require('marked');
 
 module.exports = {
   siteName: 'Matsuko Friedland',
