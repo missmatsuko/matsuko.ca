4b19ba8f2f52ee351d491bbfc9ab7de9657e217c
Matsuko Friedland <info@matsuko.ca>
2023-01-22 21:33:31 -0800

    Switch to nunjucks and set up base templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,19 @@
-const eleventyVue = require("@11ty/eleventy-plugin-vue");
-
 module.exports = function(eleventyConfig) {
-  // Use Vue templates
-  eleventyConfig.addPlugin(eleventyVue);
-
   // Define blog posts collection
-  eleventyConfig.addCollection("blogPosts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/blog/**/*.md");
-  });
+  // eleventyConfig.addCollection("blogPosts", function(collectionApi) {
+  //   return collectionApi.getFilteredByGlob("src/blog/**/*.md");
+  // });
 
-  // Define portfolio items collection
-  eleventyConfig.addCollection("portfolioItems", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/portfolio/**/*.md");
-  });
+  // // Define portfolio items collection
+  // eleventyConfig.addCollection("portfolioItems", function(collectionApi) {
+  //   return collectionApi.getFilteredByGlob("src/portfolio/**/*.md");
+  // });
 
   // Return your Object options:
   return {
     dir: {
       input: "src",
       output: "dist",
-      data: "data",
-      includes: "includes",
     },
   }
 };
