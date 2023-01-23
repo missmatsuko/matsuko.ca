a35322a48cfbab7fa0805de6703fa5024dea7bd3
Matsuko Friedland <info@matsuko.ca>
2023-01-23 13:02:34 -0800

    Format displayed date on blog

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,25 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const markdownItBlockEmbed = require("markdown-it-block-embed");
 
+const formatDate = function(UTCDate) {
+  const date = new Date(UTCDate);
+  return date.toLocaleDateString('en-CA', {
+    dateStyle: 'long',
+  });
+}
+
 module.exports = function(eleventyConfig) {
   /* TODO: sort and filter blog and portfolio items better? */
+  /* TODO: show only posts with date in the past? */
 
   // Define blog posts collection
   eleventyConfig.addCollection("blogPosts", function(collectionApi) {
     return collectionApi
       .getFilteredByGlob("src/blog/**/*.md")
+      .map((blogPost) => {
+        blogPost.data['formattedDate'] = formatDate(blogPost.data.date);
+        return blogPost;
+      })
       .sort((a, b) => {
         return b.data.date - a.data.date;
       });
@@ -20,6 +32,10 @@ module.exports = function(eleventyConfig) {
       .getFilteredByGlob("src/portfolio/**/*.md")
       .filter((portfolioItem) => {
         return portfolioItem.data.featured;
+      })
+      .map((portfolioItem) => {
+        portfolioItem.data['formattedDate'] = formatDate(portfolioItem.data.date);
+        return portfolioItem;
       })
       .sort((a, b) => {
         return b.data.date - a.data.date;
