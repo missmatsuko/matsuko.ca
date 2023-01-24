5f031dbbfc1e3000fe5f55c1beca8c5cc1f5bf9d
Matsuko Friedland <info@matsuko.ca>
2023-01-23 16:09:09 -0800

    Add Cloudinary image transforms

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,38 @@ const markdownItAnchor = require("markdown-it-anchor");
 const markdownItBlockEmbed = require("markdown-it-block-embed");
 const faviconsPlugin = require("eleventy-plugin-gen-favicons");
 
+// Returns Cloudinary URL for images
+const cloudinaryUrl = function(imagePath, options) {
+  // TODO: use site.url?
+
+  // Full options list here: https://cloudinary.com/documentation/image_transformation_reference
+  // For now only supporting: width, height
+  // There's a Cloudinary SDK that handles this, but I don't want to include the whole package
+
+  const optionsString = Object.entries(options)
+    .map(option => {
+      const key = option[0];
+      const value = option[1];
+
+      if (!value) {
+        return null;
+      }
+
+      switch(key) {
+        case 'width':
+          return `w_${value}`;
+        case 'height':
+          return `h_${value}`;
+        default:
+          return null;
+      }
+    })
+    .filter(value => value !== null)
+    .join(',');
+
+  return `https://res.cloudinary.com/dcelqcs5j/image/fetch/${optionsString ? `${optionsString},` : ''}f_auto,c_fill/https://matsuko.ca/assets${imagePath}`;
+};
+
 const formatDate = function(UTCDate) {
   const date = new Date(UTCDate);
   return date.toLocaleDateString('en-CA', {
@@ -63,6 +95,11 @@ module.exports = function(eleventyConfig) {
   // TODO: Add more content to manifest?
   eleventyConfig.addPlugin(faviconsPlugin, {
     outputDir: outputDir,
+  });
+
+  // Add Cloudinary URL shortcode
+  eleventyConfig.addShortcode("cloudinaryUrl", function(imagePath, options) {
+    return cloudinaryUrl(imagePath, options);
   });
 
   // Eleventy configuration
